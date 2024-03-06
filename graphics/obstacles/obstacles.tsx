7f1bcc5f715bbe95a20949f67919e960c545402c
Mori <laurent.ferric@gmail.com>
2024-03-06 16:54:16 +0100

    Feature - Ladder
    Updated the obstacles image and tmx file for the TILED program.
    Updated the maps tmx to use this new obstacles tileset

--- graphics/obstacles/obstacles.tsx
+++ graphics/obstacles/obstacles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="obstacles" tilewidth="16" tileheight="16" tilecount="40" columns="10">
- <image source="obstacles.png" trans="747474" width="160" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="obstacles" tilewidth="16" tileheight="16" tilecount="44" columns="11">
+ <image source="obstacles.png" trans="747474" width="176" height="64"/>
 </tileset>
