b702594c95df4650f108709e1bf942b9b2b26b60
Mori <laurent.ferric@gmail.com>
2024-03-12 17:05:43 +0100

    Feature - item pick up
    Tile set format tsx (TILED) for items, consumables and particles  have been added, for ease of generating correct CSVs

---
+++ graphics/consumables/consumables.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="consumables" tilewidth="16" tileheight="16" tilecount="40" columns="20">
+ <image source="consumables.png" trans="747474" width="320" height="32"/>
+</tileset>
---
+++ graphics/items/items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="items" tilewidth="16" tileheight="16" tilecount="72" columns="12">
+ <image source="items.png" trans="747474" width="192" height="96"/>
+</tileset>
