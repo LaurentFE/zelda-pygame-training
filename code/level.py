6ed93f290c784a6cefc541041be71c01914185a5
Mori <laurent.ferric@gmail.com>
2024-03-20 17:21:09 +0100

    Feature - Stalfos
    Dungeon spooky skeleton. Kind of dumb, but durable, and it has a sword !
    Doesn't attack, just damage by collision.
    It moves randomly for some time, then remembers the Player here, and go straight toward the Player in a straight line, too dumb to navigate around obstacles.
    When Player hits it, it makes it forget about the Player for a few seconds, before it comes back to attack you again.

--- code/level.py
+++ code/level.py
@@ -9,7 +9,7 @@ import tileset
 from tile import Tile
 from obstacle import Obstacle
 from player import Player
-from enemies import RedOctorock, RedMoblin, Zora, Leever
+from enemies import RedOctorock, RedMoblin, Stalfos, Zora, Leever
 from particles import (Heart, Rupee, CBomb, Fairy, Key, HeartReceptacle,
                        Ladder, RedCandle, Boomerang, WoodenSword)
 from selector import Selector
@@ -516,6 +516,12 @@ class Level(metaclass=Singleton):
                                   self.visible_sprites,
                                   self.obstacle_sprites,
                                   self.particle_sprites)
+                    elif sprite_id == STALFOS_WALKING_FRAME_ID:
+                        Stalfos((x, y),
+                                [self.visible_sprites, self.enemy_sprites],
+                                self.visible_sprites,
+                                self.obstacle_sprites,
+                                self.particle_sprites)
                     elif sprite_id == ZORA_WALKING_DOWN_FRAME_ID:
                         Zora((x, y),
                              [self.visible_sprites, self.enemy_sprites],
